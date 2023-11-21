eab534a28383dc169b81397fc2e654bab2cc082e
Raquel <raquelbulhoes212@gmail.com>
2023-11-21 11:34:42 -0300

    contator finalizando e Formulário 21/11

--- EventosDOM/miniApps-main/Color_Flipper/app.js
+++ EventosDOM/miniApps-main/Color_Flipper/app.js
@@ -1,1 +1,16 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "blue", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById ('btn')
+const color = document.querySelector('.color')
+
+
+btn.addEventListener('click',function(){
+// console.log(document.body)
+const numero = getRandomNumber()
+document.body.style.backgroundColor = colors[numero]
+color.textContent= colors[numero]
+
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+}
--- EventosDOM/miniApps-main/Contador/app.js
+++ EventosDOM/miniApps-main/Contador/app.js
@@ -1,12 +1,14 @@
-let count = 0
 
-const value = document.getElementById('value')
+
+const value = document.getElementById('value') // seleciona o elemento que vai sofrer uma ação ou comportamento
 const btns = document.querySelectorAll('.btn')
+
+let count = 0
 
 // console.log(value)
 // console.log(btns)
 
-btns.forEach(function(btn){
+btns.forEach(function(btn){ // forEach listar elementos
 
 btn.addEventListener('click',function(){
    const style = event.currentTarget.classList
@@ -15,13 +17,31 @@ btn.addEventListener('click',function(){
      count--
     
    }else if(style.contains ('reset')){
-   count = 0
+   count = 0;
    }else if(style.contains('increase')){
-     count++
+     count++;
    }
    value.textContent = count
+
+
+
+   if(count === 30){
+    value.style.color = 
+    value.className = 'gradiente'
+  
+   }
+
+if(count > 0){
+  // ação 
+  value.style.color = '#0f0'
+  
+}else if (count < 0){
+  value.style.color = '#f00'
+}else if (count === 0){
+  value.style.color = '#00f'
+}
+
+
 })
-
-
 
 })
